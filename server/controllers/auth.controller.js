2ca74759adbba8b90ccb2e3db9e34b505bfd5172
Aa-sheesh <aashish4912345@gmail.com>
2025-02-08 16:51:30 +0530

    refactor login function for improved error handling and formatting

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,24 +1,27 @@
 import User from "../models/user.model.js";
 
-export const login = async(req , res)=>{
-    try {
-        const {email,password}=req.body;
-        const user=await User.findOne({email});
-        // console.log(user);
-        if(user &&(await user.comparePassword(password))){
-            res.status(200).json({
-                id:user._id,
-                name:user.name,
-                email:user.email,
-                branch:user.branch,
-                section:user.section,
-            })
-        }
-        
-    } catch (error) {
-        console.log("Error in login",error);
-        res.status(500).json({
-            message:"Internal Server Error",
-        })
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    // console.log(user);
+    if (user && (await user.comparePassword(password))) {
+      res.status(200).json({
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        branch: user.branch,
+        section: user.section,
+      });
+    } else {
+      res.status(401).json({
+        message: "Invalid email or password",
+      });
     }
-}
+  } catch (error) {
+    console.log("Error in login", error);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
